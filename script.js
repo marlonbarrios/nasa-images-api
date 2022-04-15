bb632ef7c3f26d9732c101073cb2ca2a0c7d997e
marlonbarrios <marlon@dance-tech.net>
2022-04-14 22:08:43 -0400

    updated

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
+
 // NASA API
 const count = 10;
-const apiKey = 'aWwxJFzSu8y4STbpdH2VQjBWF2WN1NvyT8Qko91f';
+const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
